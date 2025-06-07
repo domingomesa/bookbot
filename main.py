dbc837092a341981b1ffa6f8c634f1e4ca44ee89
domingomesa <domingoamesa@gmail.com>
2025-06-07 11:53:27 -0500

    updated main.py: removed hardcoded books and added using the cmd for book input as sys.argv[1]

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
 from stats import get_number_of_words, get_number_of_characters, get_sorted_characters
+import sys
 
 def get_book_text(book):
     with open(book, "r", encoding="utf-8") as f:
         return f.read()
 
 def main():
-    text = get_book_text("books/frankenstein.txt")
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+
+    text = get_book_text(sys.argv[1])
     word_count = get_number_of_words(text)
     num_char = get_number_of_characters(text)
 
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
 
     print("----------- Word Count ----------")
     print(f"Found {word_count} total words")
